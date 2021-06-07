03a7c1abb39f2f45c4aa0f518bc7f558088adad0
junbaor <junbaor@gmail.com>
2021-06-08 00:09:49 +0800

    Upgrade jike-cli to v.0.1.1

--- jike-cli.rb
+++ jike-cli.rb
@@ -1,15 +1,15 @@
 class JikeCli < Formula
   desc '即刻命令行客户端'
   homepage 'https://github.com/junbaor/jike-cli'
-  url 'https://github.com/junbaor/jike-cli/releases/download/v0.1.0/jike-cli.tar.gz'
-  sha256 '82732e901648e9ecacb76598bfb6dc2d19ec35149953d53e0c7da072409c467b'
-  version "v0.1.0"
+  url 'https://github.com/junbaor/jike-cli/releases/download/v0.1.1/jike-cli.jar'
+  sha256 '08aa932e90c7700ebde91ee6dbf5c6de3c6367b73487d563ebb93d312e71bd0c'
+  version "v0.1.1"
 
   bottle :unneeded
   depends_on 'openjdk@8'
 
   def install
-    libexec.install Dir['*']
-    bin.write_jar_script libexec / 'jike-cli.jar', 'jike-cli'
+    libexec.install 'jike-cli.jar'
+    bin.write_jar_script libexec/'jike-cli.jar', 'jike-cli'
   end
 end
